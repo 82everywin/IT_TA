a8c588cdd4aac8fa5283585b3771e398fdd91b36
D00\Administrator <82everywin@gmail.com>
2023-11-21 19:53:26 +0900

    no message

---
+++ src/Swing/TextAreaEx.java
@@ -0,0 +1,17 @@
+package Swing;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class TextAreaEx extends JFrame{
+	
+	private JTextField jf = new JTextField(20);
+	private JTextArea tf = new  JTextArea(7,20);
+	
+	public TextAreaEx() {
+		setTitle("텍스트 영역 만들기 예제");
+		setDefaultCloseOperation(J)
+	}
+
+}
